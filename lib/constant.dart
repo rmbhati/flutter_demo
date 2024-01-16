a56a4b48e283bef547d71c4e53d460d888e7fd78
rmbhati <ramesh1991>
2024-01-16 14:24:28 +0530

    validation on form

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,8 +1,33 @@
+import 'package:flutter/material.dart';
+
 class ApiConstants {
-
-
   //http://kgkdam.com:3910/kgkapi/smartservice/slogin/50226/LiveTest1234
 
   static String baseUrl = 'http://kgkdam.com:3910/kgkapi';
   static String usersEndpoint = '/smartservice/slogin/';
+
+  static alertDialog(BuildContext context, String message) {
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {},
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      //title: Text("My title"),
+      content: Text(message),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
 }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,9 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-
+import '../constant.dart';
 import '../model/login_model.dart';
-import '../model/user_model.dart';
 import '../service/api_service.dart';
 
 class Login extends StatefulWidget {
@@ -14,104 +11,104 @@ class Login extends StatefulWidget {
 }
 
 class LoginState extends State<Login> {
-  showAlertDialog(BuildContext context, LoginModel userModel) {
-    // set up the button
-    Widget okButton = TextButton(
-      child: Text("OK"),
-      onPressed: () {},
-    );
+  final _formKey = GlobalKey<FormState>();
 
-    String ddd = "${userModel.sts} ${userModel.message}\nINFO\n${userModel.data?[0].empID} ${userModel.data?[0].fullName}";
-
-    // set up the AlertDialog
-    AlertDialog alert = AlertDialog(
-      title: Text("My title"),
-      content: Text(ddd),
-      actions: [
-        okButton,
-      ],
-    );
-
-    // show the dialog
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
+      body: Form(
+          key: _formKey,
           child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-            child: SizedBox(
-                width: 200, height: 200, child: Image.asset('assets/kgk.png')),
-          ),
-          const Text(
-            "Digital Jewelry Library",
-            style: TextStyle(
-                color: Colors.black, fontSize: 22, fontFamily: 'Mulish'),
-          ),
-          const Padding(
-            padding:
-                EdgeInsets.only(left: 20.0, right: 20.0, top: 15, bottom: 0),
-            child: TextField(
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'User name or Email'),
-              style: TextStyle(color: Colors.black, fontFamily: 'Mulish'),
-            ),
-          ),
-          const Padding(
-            padding:
-                EdgeInsets.only(left: 20.0, right: 20.0, top: 15, bottom: 15),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Password'),
-              style: TextStyle(color: Colors.black, fontFamily: 'Mulish'),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              //TODO FORGOT PASSWORD SCREEN GOES HERE
-            },
-            child: const Text(
-              'Forgot Password?',
-              style: TextStyle(color: Colors.black, fontFamily: "Mulish"),
-            ),
-          ),
-          Container(
-            width: 250,
-            decoration: BoxDecoration(
-                color: const Color(0xFFEFEFEF),
-                borderRadius: BorderRadius.circular(10)),
-            margin: const EdgeInsets.only(
-                left: 20.0, right: 20.0, top: 10, bottom: 0.0),
-            child: TextButton(
-              onPressed: () async {
-                /*Navigator.push(context,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('assets/kgk.png')),
+              ),
+              const Text(
+                "Digital Jewelry Library",
+                style: TextStyle(
+                    color: Colors.black, fontSize: 22, fontFamily: 'Mulish'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 15, bottom: 0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter userid';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Userid'),
+                  style: const TextStyle(
+                      color: Colors.black, fontFamily: 'Mulish'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 15, bottom: 15),
+                child: TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter password';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Password'),
+                  style: const TextStyle(
+                      color: Colors.black, fontFamily: 'Mulish'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  //TODO FORGOT PASSWORD SCREEN GOES HERE
+                },
+                child: const Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Colors.black, fontFamily: "Mulish"),
+                ),
+              ),
+              Container(
+                width: 250,
+                decoration: BoxDecoration(
+                    color: const Color(0xFFEFEFEF),
+                    borderRadius: BorderRadius.circular(10)),
+                margin: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 10, bottom: 0.0),
+                child: TextButton(
+                  onPressed: () async {
+                    /*Navigator.push(context,
                             MaterialPageRoute(builder: (_) => HomePage()));*/
 
-                LoginModel loginModel =
-                    (await ApiService().getLoginData("50226", "LiveTest1234"));
-
-                showAlertDialog(context, loginModel);
-              },
-              child: const Text(
-                'LOG IN',
-                style: TextStyle(color: Colors.black, fontFamily: "Mulish"),
+                    if (_formKey.currentState!.validate()) {
+                      LoginModel userModel = (await ApiService()
+                          .getLoginData("50226", "LiveTest1234"));
+                      String ddd =
+                          "${userModel.sts} ${userModel.message}\nINFO\n${userModel.data?[0].empID} ${userModel.data?[0].fullName}";
+                      ApiConstants.alertDialog(context, ddd);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Please enter all details')),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'LOG IN',
+                    style: TextStyle(color: Colors.black, fontFamily: "Mulish"),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
-      )),
+            ],
+          )),
       //],
       //),
       //)
