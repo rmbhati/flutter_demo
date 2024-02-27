01c0933432951d836ab7c148e73a173d330e1977
rmbhati <ramesh1991>
2024-02-27 14:19:03 +0530

    search on list

--- lib/screen/approval_list.dart
+++ lib/screen/approval_list.dart
@@ -16,8 +16,10 @@ class ApprovalsList extends StatefulWidget {
 
 class ApprovalsState extends State<ApprovalsList> {
   int userId = 0;
-  List<ApproveData> data = [];
   late Size size;
+  TextEditingController controller = TextEditingController();
+  List<ApproveData> data = [];
+  List<ApproveData> searchData = [];
 
   @override
   void initState() {
@@ -49,10 +51,24 @@ class ApprovalsState extends State<ApprovalsList> {
     }
   }
 
+  onSearchTextChanged(String text) async {
+    searchData.clear();
+    if (text.isEmpty) {
+      setState(() {});
+      return;
+    }
+    for (var userDetail in data) {
+      if (userDetail.code.toLowerCase().contains(text.toLowerCase())) {
+        searchData.add(userDetail);
+      }
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
-   Orientation orientation = MediaQuery.of(context).orientation;
+    Orientation orientation = MediaQuery.of(context).orientation;
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
@@ -61,7 +77,8 @@ class ApprovalsState extends State<ApprovalsList> {
               icon: Icon(Icons.arrow_back,
                   size: orientation == Orientation.portrait
                       ? size.height * 0.025
-                      : size.width * 0.02, color: Colors.white),
+                      : size.width * 0.02,
+                  color: Colors.white),
               onPressed: () => Navigator.of(context).pop(),
             ),
             title: Text(widget.title,
@@ -73,35 +90,103 @@ class ApprovalsState extends State<ApprovalsList> {
                         : size.width * 0.018)),
             automaticallyImplyLeading: false,
             elevation: 10,
-            shape:
-            const Border(bottom: BorderSide(color: Colors.black, width: 0.1))),
-        body: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  Constants.snackBar(context, data[index].code, size);
-                },
-                child: Card(
-                  margin: EdgeInsets.all( orientation == Orientation.portrait
-                      ? size.height * 0.008
-                      : size.width * 0.005),
-                  child: Padding(
-                    padding: EdgeInsets.all( orientation == Orientation.portrait
-                        ? size.height * 0.02
-                        : size.width * 0.015),
-                    child: Text("${data[index].code} (${data[index].count})",
-                        style: TextStyle(
-                            letterSpacing: 1,
-                            color: Colors.black54,
-                            fontFamily: "Mulish",
-                            fontWeight: FontWeight.w600,
-                            fontSize:  orientation == Orientation.portrait
-                                ? size.height * 0.02
-                                : size.width * 0.015)),
-                  ),
+            shape: const Border(
+                bottom: BorderSide(color: Colors.black, width: 0.1))),
+        body: Column(
+          children: [
+            Card(
+              elevation: 5,
+              //color: Colors.blue,
+              margin: EdgeInsets.all(orientation == Orientation.portrait
+                  ? size.height * 0.008
+                  : size.width * 0.005),
+              child: ListTile(
+                leading: const Icon(Icons.search),
+                title: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                      hintText: 'Search', border: InputBorder.none),
+                  onChanged: onSearchTextChanged,
                 ),
-              );
-            })); // Here you direct access using widget
+                trailing: IconButton(
+                  icon: const Icon(Icons.cancel),
+                  onPressed: () {
+                    controller.clear();
+                    FocusScope.of(context).unfocus();//hide keyboard
+                    onSearchTextChanged('');
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: searchData.isNotEmpty || controller.text.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: searchData.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Constants.snackBar(
+                                context, searchData[index].code, size);
+                          },
+                          child: Card(
+                            margin: EdgeInsets.all(
+                                orientation == Orientation.portrait
+                                    ? size.height * 0.008
+                                    : size.width * 0.005),
+                            child: Padding(
+                              padding: EdgeInsets.all(
+                                  orientation == Orientation.portrait
+                                      ? size.height * 0.02
+                                      : size.width * 0.015),
+                              child: Text(
+                                  "${searchData[index].code} (${searchData[index].count})",
+                                  style: TextStyle(
+                                      letterSpacing: 1,
+                                      color: Colors.black54,
+                                      fontFamily: "Mulish",
+                                      fontWeight: FontWeight.w600,
+                                      fontSize:
+                                          orientation == Orientation.portrait
+                                              ? size.height * 0.02
+                                              : size.width * 0.015)),
+                            ),
+                          ),
+                        );
+                      })
+                  : ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Constants.snackBar(context, data[index].code, size);
+                          },
+                          child: Card(
+                            margin: EdgeInsets.all(
+                                orientation == Orientation.portrait
+                                    ? size.height * 0.008
+                                    : size.width * 0.005),
+                            child: Padding(
+                              padding: EdgeInsets.all(
+                                  orientation == Orientation.portrait
+                                      ? size.height * 0.02
+                                      : size.width * 0.015),
+                              child: Text(
+                                  "${data[index].code} (${data[index].count})",
+                                  style: TextStyle(
+                                      letterSpacing: 1,
+                                      color: Colors.black54,
+                                      fontFamily: "Mulish",
+                                      fontWeight: FontWeight.w600,
+                                      fontSize:
+                                          orientation == Orientation.portrait
+                                              ? size.height * 0.02
+                                              : size.width * 0.015)),
+                            ),
+                          ),
+                        );
+                      }),
+            )
+          ],
+        )); // Here you direct access using widget
   }
 }
